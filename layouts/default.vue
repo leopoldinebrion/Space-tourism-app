59a246a3973907a125b10b622bc55da130449028
Léopoldine Brion <leopoldine.brion@outlook.fr>
2022-02-22 19:08:02 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <div class="logo"><img src="@/assets/shared/logo.svg" alt="Logo" /></div>
+        <ul class="links">
+          <li>
+            <NuxtLink to="/"><span>00</span> HOME</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"><span>01</span> DESTINATION</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"><span>02</span> CREW</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"><span>03</span> TECHNOLOGY</NuxtLink>
+          </li>
+        </ul>
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<style scoped>
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 2.4rem;
+  padding-left: 2.4rem;
+}
+
+.links {
+  display: flex;
+  justify-content: space-between;
+  width: 40%;
+  padding: 39px 48px;
+  background-color: #ffffff0d;
+  backdrop-filter: blur(1.5rem);
+  letter-spacing: 1.8px;
+  font-size: 16px;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home">
+        <div class="home-img"><img src="../assets/home/background-home-desktop.jpg" alt=""></div>
+        <div class="home-content">
+            <p>So, you want to travel to</p>
+            <h1>Space</h1>
+            <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience!</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    
+    .home-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: -1;
+        object-fit: cover;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+</style>
+
